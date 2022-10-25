ec6ccaebe392d9653ca97b47d065525a613d05cb
Jonathan_franco <jonathan.franco@epitech.eu>
2022-10-25 19:12:46 +0200

    left-nav component added

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,11 +4,13 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { HeadersComponent } from './headers/headers.component';
+import { LeftNavComponent } from './left-nav/left-nav.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    HeadersComponent
+    HeadersComponent,
+    LeftNavComponent
   ],
   imports: [
     BrowserModule,
